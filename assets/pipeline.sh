c20ecc13482d7cb32a2a1ec8acab69b4c43ebdcd
ba77 <ba77@lab-rg07-10.cms.waikato.ac.nz>
2022-05-27 16:05:53 +1200

    COMPX341-22A-A3 Commiting from CI/CD Pipeline

--- assets/pipeline.sh
+++ assets/pipeline.sh
@@ -1,3 +1,12 @@
 npm install
-npm run build
+
+if npm run build ; then
+    echo "Build succeeded"
+    git add *
+    git commit -m "COMPX341-22A-A3 Commiting from CI/CD Pipeline"
+    git push
+else
+    echo "Command failed"
+fi
+
 npm run start
