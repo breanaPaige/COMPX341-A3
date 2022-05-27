734d935c8723ae6ed74fab0d3e3e4fa602f8a53d
ba77 <ba77@lab-rg07-10.cms.waikato.ac.nz>
2022-05-27 16:48:15 +1200

    COMPX341-22A-A3 COMPX341-22A-Maintenance commit testing for first time

--- assets/pipeline.sh
+++ assets/pipeline.sh
@@ -1,3 +1,9 @@
+
+for x in *.ts; do
+    [ -f "$x" ] || break
+    echo '// Breana Ahern 1533265' | cat - $x > temp && mv temp $x
+done
+
 if [ "$#" -ne 1 ] ; then
     echo "Please provide a commit message"
 
@@ -9,6 +15,7 @@ else
         git add *
         git commit -m "$1"
         git push
+        
     else
         echo "Command failed"
     fi
@@ -16,4 +23,3 @@ else
     npm run start
 
 fi
-
